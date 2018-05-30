ce79e810f7be0a744365487282e9f94938f3ba97
Hadeel <hadeelalomary66@gmail.com>
2018-05-30 11:29:34 +0300

    create hero class and add logic to the heroes list component

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
 
 @Component({
   selector: 'app-heros',
@@ -6,7 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heros.component.css']
 })
 export class HerosComponent implements OnInit {
-
+  heros: Hero[] = [
+    { id: 1, name: 'Hadeel' },
+    { id: 2, name: 'Amer' },
+    { id: 4, name: 'Omar' },
+    { id: 6, name: 'Ameen' },
+    { id: 10, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' },
+    { id: 22, name: 'Alaa' }
+  ];
   constructor() { }
 
   ngOnInit() {
