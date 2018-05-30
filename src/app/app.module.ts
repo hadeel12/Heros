206175f2edaba04722b01929482b3d9f3f354fcc
Hadeel <hadeelalomary66@gmail.com>
2018-05-30 14:43:12 +0300

    Add logic to the hero details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HerosComponent } from './heros/heros.component';
 import { HeroDetailsComponent } from './hero-details/hero-details.component';
@@ -12,7 +12,8 @@ import { HeroDetailsComponent } from './hero-details/hero-details.component';
     HeroDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
