0360e9210e38b690ec48f50a54476c50398a587c
Hadeel <hadeelalomary66@gmail.com>
2018-05-30 12:32:57 +0300

    Create hero details component (html + css)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HerosComponent } from './heros/heros.component';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HerosComponent
+    HerosComponent,
+    HeroDetailsComponent
   ],
   imports: [
     BrowserModule
