bdc653c6323efe2082056e188e4d1dbbaaaef3e1
Hadeel <hadeelalomary66@gmail.com>
2018-05-30 11:40:21 +0300

    enhance hero component ( add herodata class and use it in hero component as hero data )

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero';
+import { Herodata } from '../herodata';
 
 @Component({
   selector: 'app-heros',
@@ -7,23 +7,7 @@ import {Hero} from '../hero';
   styleUrls: ['./heros.component.css']
 })
 export class HerosComponent implements OnInit {
-  heros: Hero[] = [
-    { id: 1, name: 'Hadeel' },
-    { id: 2, name: 'Amer' },
-    { id: 4, name: 'Omar' },
-    { id: 6, name: 'Ameen' },
-    { id: 10, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' },
-    { id: 22, name: 'Alaa' }
-  ];
+  heros = Herodata;
   constructor() { }
 
   ngOnInit() {
