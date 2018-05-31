4c5eb9e271a9249f6cf1e92f2c5c562876d9eda8
Hadeel <hadeelalomary66@gmail.com>
2018-05-31 11:45:53 +0300

    ***** change the position of hero detail component in heros.component .

--- src/app/heroes.ts
+++ src/app/heroes.ts
@@ -1,11 +1,11 @@
 import {Hero} from './hero';
 
 export const Heroes: Hero[] = [
-  { id: 1, name: 'Hadeel' },
-  { id: 2, name: 'Amer' },
-  { id: 4, name: 'Omar' },
-  { id: 6, name: 'Ameen' },
-  { id: 10, name: 'Mr. Nice' },
+  // { id: 1, name: 'Hadeel' },
+  // { id: 2, name: 'Amer' },
+  // { id: 4, name: 'Omar' },
+  // { id: 6, name: 'Ameen' },
+  // { id: 10, name: 'Mr. Nice' },
   { id: 12, name: 'Narco' },
   { id: 13, name: 'Bombasto' },
   { id: 14, name: 'Celeritas' },
