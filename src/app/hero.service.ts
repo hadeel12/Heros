e12e01d4ec19358a172c9b6fe056efd5b76bd115
Hadeel <hadeelalomary66@gmail.com>
2018-06-03 12:21:19 +0300

    update hero service to use HttpClient

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
-import { Heroes} from './heroes';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Hero} from './hero';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  heroes = Heroes;
-  constructor() { }
-  getHero() {
-    return this.heroes;
+ private heroUrl = 'api/heroes';
+  constructor(private http: HttpClient) { }
+
+  getHeroes(): Observable<Hero[]>  {
+    return this.http.get<Hero[]>(this.heroUrl);
   }
-  getById(id: number) {
-     return this.heroes.find(h => h.id === id);
+  getById(id: number): Observable<Hero> {
+     return this.http.get<Hero>(this.heroUrl + '/' + id);
   }
 }
