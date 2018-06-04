431b97d2715de0d46241d604f09d5eeb7e2f5345
Hadeel <hadeelalomary66@gmail.com>
2018-06-04 15:28:45 +0300

    Add logic to the search component

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Subject} from 'rxjs';
+import {Hero} from '../hero';
+import {debounceTime, distinctUntilChanged, switchMap} from 'rxjs/internal/operators';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  constructor(private heroService:HeroService) { }
 
-  constructor() { }
+  private searchTerms = new Subject<string>();
+  heroes: Hero;
 
   ngOnInit() {
+    this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term:string)=>this.heroService.searchHero(term))
+    ).subscribe(hero => this.heroes = hero)
   }
+
+  search(term: string) {
+    this.searchTerms.next(term);
+  }
+
 
 }
