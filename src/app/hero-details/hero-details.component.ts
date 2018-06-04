baa07dc6fb8920cd9f6a3224adcd3a56bf1f94fc
Hadeel <hadeelalomary66@gmail.com>
2018-06-04 10:58:54 +0300

    active goback function in hero detail

--- src/app/hero-details/hero-details.component.ts
+++ src/app/hero-details/hero-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
 import { HeroService } from '../hero.service';
 import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-hero-details',
@@ -12,7 +13,8 @@ export class HeroDetailsComponent implements OnInit {
   hero: Hero;
   constructor(
   private heroService: HeroService,
-  private route: ActivatedRoute) {
+  private route: ActivatedRoute,
+  private location: Location) {
   }
 
   ngOnInit() {
@@ -24,7 +26,10 @@ export class HeroDetailsComponent implements OnInit {
     this.heroService.getById(id).subscribe(hero => this.hero = hero);
   }
   updateHero() {
-    this.heroService.updateHero(this.hero).subscribe();
+    this.heroService.updateHero(this.hero).subscribe(h =>this.goBack());
+  }
+  goBack() {
+    this.location.back();
   }
 
 }
