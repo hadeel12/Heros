42b678bd69801e4d9f5369a402993b3a35f6c26b
Hadeel <hadeelalomary66@gmail.com>
2018-06-04 10:45:48 +0300

    update name of heroUrl => heroesUrl and create delete http request

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,26 +1,31 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Hero} from './hero';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
- private heroUrl = 'api/heroes';
+ private heroesUrl = 'api/heroes';
   constructor(private http: HttpClient) { }
 
   getHeroes(): Observable<Hero[]>  {
-    return this.http.get<Hero[]>(this.heroUrl);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
   getById(id: number): Observable<Hero> {
-     return this.http.get<Hero>(this.heroUrl + '/' + id);
+     return this.http.get<Hero>(this.heroesUrl + '/' + id);
   }
   addHero(hero: Hero): Observable<Hero> {
-    return this.http.post<Hero>(this.heroUrl , hero);
+    return this.http.post<Hero>(this.heroesUrl , hero);
   }
   updateHero(hero: Hero) {
-    return this.http.put(this.heroUrl , hero);
+    return this.http.put(this.heroesUrl , hero);
   }
- 
+  deleteHero(hero: Hero): Observable<Hero> {
+    const id = hero.id ;
+    return this.http.delete<Hero>(this.heroesUrl+'/'+id);
+  }
+
 }
